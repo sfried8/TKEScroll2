16522145399cace8fa9643adafee1fd0788bf5a2
Sam Friedman <sfried8@gmail.com>
2019-07-12 22:26:07 -0400

    added workbox options

--- quasar.conf.js
+++ quasar.conf.js
@@ -3,11 +3,11 @@
 module.exports = function(ctx) {
   return {
     // app plugins (/src/boot)
-    boot: ["customComponents", "routerAuthentication", "gtm-boot"],
-    css: ["app.styl"],
+    boot: ['customComponents', 'routerAuthentication', 'gtm-boot'],
+    css: ['app.styl'],
     extras: [
-      "roboto-font",
-      "material-icons" // optional, you are not bound to it
+      'roboto-font',
+      'material-icons', // optional, you are not bound to it
       // 'ionicons-v4',
       // 'mdi-v3',
       // 'fontawesome-v5'
@@ -22,96 +22,99 @@ module.exports = function(ctx) {
       // extractCSS: false,
       extendWebpack(cfg) {
         cfg.module.rules.push({
-          enforce: "pre",
+          enforce: 'pre',
           test: /\.(js|vue)$/,
-          loader: "eslint-loader",
-          exclude: /(node_modules|quasar)/
+          loader: 'eslint-loader',
+          exclude: /(node_modules|quasar)/,
         });
-      }
+      },
     },
     devServer: {
       // https: true,
       // port: 8080,
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
     },
     // framework: 'all' --- includes everything; for dev only!
     framework: {
       components: [
-        "QBtn",
-        "QDrawer",
-        "QHeader",
-        "QIcon",
-        "QInput",
-        "QItem",
-        "QItemLabel",
-        "QItemSection",
-        "QLayout",
-        "QList",
-        "QMenu",
-        "QPage",
-        "QPageContainer",
-        "QPageScroller",
-        "QPageSticky",
-        "QSelect",
-        "QSeparator",
-        "QTab",
-        "QTabPanel",
-        "QTabPanels",
-        "QTabs",
-        "QToggle",
-        "QToolbar",
-        "QToolbarTitle"
+        'QBtn',
+        'QDrawer',
+        'QHeader',
+        'QIcon',
+        'QInput',
+        'QItem',
+        'QItemLabel',
+        'QItemSection',
+        'QLayout',
+        'QList',
+        'QMenu',
+        'QPage',
+        'QPageContainer',
+        'QPageScroller',
+        'QPageSticky',
+        'QSelect',
+        'QSeparator',
+        'QTab',
+        'QTabPanel',
+        'QTabPanels',
+        'QTabs',
+        'QToggle',
+        'QToolbar',
+        'QToolbarTitle',
       ],
-      directives: ["Ripple", "TouchPan"],
+      directives: ['Ripple', 'TouchPan'],
       // Quasar plugins
-      plugins: ["Notify", "LocalStorage", "Loading", "Dialog"]
+      plugins: ['Notify', 'LocalStorage', 'Loading', 'Dialog'],
       // iconSet: ctx.theme.mat ? 'material-icons' : 'ionicons'
       // lang: 'de' // Quasar language
     },
     // animations: 'all' --- includes all animations
     animations: [],
     ssr: {
-      pwa: false
+      pwa: false,
     },
     pwa: {
       // workboxPluginMode: 'InjectManifest',
-      // workboxOptions: {},
+      workboxOptions: {
+        skipWaiting: true,
+        clientsClaim: true,
+      },
       manifest: {
-        name: "TKE Scroll",
-        short_name: "TKE Scroll",
+        name: 'TKE Scroll',
+        short_name: 'TKE Scroll',
         // description: 'Best PWA App in town!',
-        display: "standalone",
-        orientation: "portrait",
-        background_color: "#ffffff",
-        theme_color: "#AD2624",
+        display: 'standalone',
+        orientation: 'portrait',
+        background_color: '#ffffff',
+        theme_color: '#AD2624',
         icons: [
           {
-            src: "statics/icons/icon-128x128.png",
-            sizes: "128x128",
-            type: "image/png"
+            src: 'statics/icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
           },
           {
-            src: "statics/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
+            src: 'statics/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "statics/icons/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png"
+            src: 'statics/icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
           },
           {
-            src: "statics/icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png"
+            src: 'statics/icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
           },
           {
-            src: "statics/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
+            src: 'statics/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
     },
     cordova: {
       // id: 'org.cordova.quasar.app'
@@ -134,7 +137,7 @@ module.exports = function(ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
         // appId: 'quasar-app'
-      }
-    }
+      },
+    },
   };
 };
