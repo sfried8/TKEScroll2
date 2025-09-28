e4ab18e9c835eb14b96dce2b6d3674c7c8caffb7
Sam Friedman <sfried8@gmail.com>
2025-09-28 12:35:25 -0400

    added new fields to historaddbrother

--- src/pages/HistorAddBrother.vue
+++ src/pages/HistorAddBrother.vue
@@ -3,8 +3,12 @@
     <div class="row q-col-gutter-lg no-wrap">
       <q-input class="col" v-model="scroll" label="Scroll" type="number" />
       <q-input class="col" v-model="pc" label="Pledge Class" type="number" />
+      <q-input v-model="year" class="col" label="Year" type="number" />
     </div>
+    <div class="row no-wrap q-col-gutter-lg">
 
+      <q-select v-model="type" class="col" label="Member Type" :options="memberTypes" />
+    </div>
     <div class="row q-col-gutter-lg no-wrap">
       <q-input class="col" v-model="fname" label="First Name" />
       <q-input class="col" v-model="lname" label="Last Name" />
@@ -16,6 +20,7 @@
     </div>
 
     <q-toggle v-model="active" label="Active" />
+    <q-select v-model="achievements" multiple class="col" label="Achievements" :options="achievementTypes" />
     <br />
     <q-btn @click="submit" icon="add">Add</q-btn>
     <br /><br />
@@ -28,11 +33,18 @@
 </template>
 
 <script>
+import { PrettyType, TYPES } from '../model/Enums';
+import { PrettyAchievement } from '../model/Enums';
+
 const DEBUG = false;
 export default {
   data() {
     return {
+      year: null,
+      type: { label: PrettyType.BROTHER, value: TYPES.BROTHER },
+      achievements: [],
       scroll: 0,
+      highestScroll: 0,
       fname: "",
       lname: "",
       nickname: "",
@@ -42,6 +54,20 @@ export default {
       searcher: null,
       pendingBrothers: []
     };
+  },
+  computed: {
+    memberTypes() {
+      return Object.keys(PrettyType).map(k => ({
+        label: PrettyType[k],
+        value: k
+      }));
+    },
+    achievementTypes() {
+      return Object.keys(PrettyAchievement).map(k => ({
+        label: PrettyAchievement[k],
+        value: k
+      }));
+    }
   },
   methods: {
     addAll() {
@@ -61,30 +87,36 @@ export default {
     },
     submit() {
       const brother = {
-        scroll: `${this.scroll}`,
+        scroll: this.scroll,
         fname: this.fname,
         lname: this.lname,
         pc: +this.pc,
+        year: this.year,
+        achievements: this.achievements.map(a => a.value),
+        type: this.type.value,
         nickname: this.nickname,
-        bigS: this.big.scroll,
+        bigId: this.big.id,
         active: this.active,
         isZetaTau: this.pc < 0
       };
       this.pendingBrothers.push(brother);
-      this.scroll++;
+      if (this.scroll) {
+        this.scroll++;
+      }
       this.fname = "";
       this.lname = "";
       this.nickname = "";
+      this.achievements = [];
       this.big = null;
 
       // Brothers.addBrother(brother);
-    }
+    },
   },
   mounted() {
     this.$members.getScroll().then(brothers => {
-      const highestScroll = Math.max(...brothers.map(b => +(b?.scroll ?? 0)));
+      this.highestScroll = Math.max(...brothers.map(b => +(b?.scroll ?? 0)));
       const highestPC = Math.max(...brothers.map(b => (b && b.pc < 999 ? b.pc : 0)));
-      this.scroll = highestScroll + 1;
+      this.scroll = this.highestScroll + 1;
       this.pc = highestPC;
     });
   }
